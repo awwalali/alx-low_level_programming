fd5fb48533ab5dffda109242f445327aeb7b4ab0
awoll ali <ibnama09@gmail.com>
2022-09-08 15:27:08 -0700

    task 4, first ever code in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*This progam prints the string inthe put function */
+
+/* It ends with return(0) */
+
+int main(void)
+{
+	puts("/"Programming is like building a multilingual puzzle");
+	return(0);
+}
