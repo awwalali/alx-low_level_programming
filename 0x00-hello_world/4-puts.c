738b9bf778e5c360ddd9bbd390c375dd173b81a1
awoll ali <ibnama09@gmail.com>
2022-09-08 15:39:21 -0700

    task 4, The first ever c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,23 @@
-#include <stdio.h>
+#include <stdio.h>                                                                                                                                     
 
-/**
-* The code prints the string inside the puts function
-* The code can't use the printf() function
-* It must terminate with a value of 0
-*/
+                                                                                                                                                       
 
-int main(void)
-{
-	puts("\"Programming is like bulding a multilingual puzzle");
-	return(0);
+/**                                                                                                                                                    
+
+* The code prints the string inside the puts function                                                                                                  
+
+* The code can't use the printf() function                                                                                                             
+
+* It must terminate with a value of 0                                                                                                                  
+
+*/                                                                                                                                                     
+
+                                                                                                                                                       
+
+int main(void)                                                                                                                                         
+
+{                                                                                                                                                      
+
+        puts("\"Programming is like bulding a multilingual puzzle")
+	return (0);
 }
