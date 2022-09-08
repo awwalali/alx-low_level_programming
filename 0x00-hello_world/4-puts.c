88a9e7775c0f75a5501af235c8b4fd81bfc1008f
awoll ali <ibnama09@gmail.com>
2022-09-08 15:34:45 -0700

    task 4, The first ever code in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
-/*This progam prints the string inthe put function */
-
-/* It ends with return(0) */
+/**
+* The code prints the string inside the puts function
+* The code can't use the printf() function
+* It must terminate with a value of 0
+*/
 
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like bulding a multilingual puzzle");
 	return(0);
 }
