b4d4426a84dae80bdccd058512e15ea3001472ec
awoll ali <ibnama09@gmail.com>
2022-09-08 16:14:24 -0700

    task 5, The second code using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* The program must print the strint inside the printf function
+*
+* The programe cannot use the puts function
+* It should terminate withvalue 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
