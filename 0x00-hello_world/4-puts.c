c78631bac077a759ee828ab04343ba027b0bc022
awoll ali <ibnama09@gmail.com>
2022-09-08 16:06:31 -0700

    task 4, THhe first c code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,9 @@
 
 /**
 * The program must print the string inside the puts function
-* The printf function cannot be used
-* It should end with value 0
+*
+*The printf function cannot be used
+* It should  end with value 0
 */
 int main(void)
 {
