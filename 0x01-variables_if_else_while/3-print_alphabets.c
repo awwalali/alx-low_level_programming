d3d2d6832e4a69ef95434fdb523375195b0f5621
awoll ali <ibnama09@gmail.com>
2022-09-09 16:57:28 -0700

    task 3. lowercase and upper case alphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*main_printing the alphabet*/
+/**/
+/*description- print out the alphabet only using the putcha dunction*/
+/*Return: 0*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch ='A' ; ch <= 'Z' ; ch==)
+	{
+		putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
+
