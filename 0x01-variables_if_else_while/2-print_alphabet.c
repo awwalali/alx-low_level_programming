0a6631e9e5d3f1b70c7c1c633dea0d1e6d5afb52
awoll ali <ibnama09@gmail.com>
2022-09-09 16:49:26 -0700

    task 2. Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*main_printing the alphabet*/
+/**/
+/*description- print out the alphabet only using the putcha dunction*/
+/*Return: 0*/
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch < 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+putchar('\n');
+return (0);
+}
+
