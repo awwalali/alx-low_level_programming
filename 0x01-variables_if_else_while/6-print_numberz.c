527f37a087ce5d39aa41c89e68c65418d5d708bd
awoll ali <ibnama09@gmail.com>
2022-09-11 14:24:22 -0700

    task 6, 0-9'

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,21 +3,20 @@
 /**
  * main - print all the sigle digit numbers of base 10
  *
- * description: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.
- * You are not allowed to use any variable of type char
- * You can only use the putchar function (every other function (printf, puts, etc…) is forbidden)
- * You can only use putchar twice in your code
- * return: 0
+ * Description: Write a program that prints all single digit numbers
+ * of base 10 starting from 0
+ * You can only use the putchar function
+ * Return: 0
  */
 int main(void)
 {
-	char i;
+	int i;
 
-	for(i = '0'; i <= '9'; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
 		putchar(i);
-		putchar('\n');
 	}
+	putchar('\n');
 	return (0);
 }
 
