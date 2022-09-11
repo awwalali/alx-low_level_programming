5fcf54528e9eb3c15aa819c49f039896aff1ef36
awoll ali <ibnama09@gmail.com>
2022-09-11 14:09:07 -0700

    task 7

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -6,7 +6,7 @@
  * Description: Write a program that prints all single digit numbers of base 10 starting from 0, followed by a new line
  * Return: 0
  */
-int main(void)
+int main (void)
 {
 	char c;
 
