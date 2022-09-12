673b247f276d48de8f2a430e79f734047feb389c
awoll ali <ibnama09@gmail.com>
2022-09-12 12:52:16 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,7 +3,8 @@
 /**
  * main - print out data type sizes
  *
- * Description: Write a C program that prints the size of various types on the computer it is compiled and run on.
+ * Description: Write a C program that prints the size of various
+ * types on the computer it is compiled and run on.
  * Return: 0
  */
 int main(void)
@@ -16,8 +17,8 @@ int main(void)
 
 	printf("Size a of char: %ld byte(s)\n", sizeof(c));
 	printf("Size of an int: %ld byte(s)\n", sizeof(i));
-	printf("Size of a long: %ld byte(s)\n", sizeof(li));
-	printf("Size of a long long: %ld byte(s)\n", sizeof(lli));
+	printf("Size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %ld byte(s)\n", sizeof(lli));
 	printf("Size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
