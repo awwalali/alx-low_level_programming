deeadeda051c67ef28f2d274a3a62339e4f555bc
awoll ali <ibnama09@gmail.com>
2022-09-13 16:18:11 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - print the alphabets
+ *
+ * Description: lowercase alphabets
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a', c <= 'z', c++)
+	{
+		_putchar('c');
+	}
+	_putchar('\n');
+	return (0);
+}
