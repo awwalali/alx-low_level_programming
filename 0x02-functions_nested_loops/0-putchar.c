a66e795472b93dc95e957c65ad66dc680bc2647c
awoll ali <ibnama09@gmail.com>
2022-09-13 16:09:14 -0700

    task 0, very unsettling

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,13 +1,20 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - print out _putchar
+ * main - print putchar
  *
  * Description:
  * Return: 0
  */
 int main(void)
 {
-	printf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	return (0);
 }
