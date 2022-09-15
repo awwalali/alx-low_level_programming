2082e48b1ccbd7402e120bb83652c3a5497cc430
awoll ali <ibnama09@gmail.com>
2022-09-15 13:47:55 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - pints numbers from 0-9
+ * Return: result
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
