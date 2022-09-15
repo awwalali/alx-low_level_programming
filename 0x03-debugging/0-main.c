51a191fa47217fb06fac665f5ce78dd7e7d26187
awoll ali <ibnama09@gmail.com>
2022-09-15 09:00:51 -0700

    task 0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - tests function for positive, negative or zero
+ *
+ * Description: test only not solve
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	
+	i = 0;
+
+	positive_or_negative(i);
+	return (0);
+}
