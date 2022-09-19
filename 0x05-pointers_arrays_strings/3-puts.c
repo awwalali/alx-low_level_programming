c629d1149e56c82cab6503c08db2a1dcec722697
awoll ali <ibnama09@gmail.com>
2022-09-19 15:01:55 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts - print a strig to stdout
+ * @str: The string to be printed
+ * Return: The stringto be printed
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		_putchar(*(str + i));
+		i++;
+	}
+	_putchar(10);
+}
