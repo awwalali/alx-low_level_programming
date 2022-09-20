bbb5dafd200c649b6723cd892ccc89941eff3ed9
awoll ali <ibnama09@gmail.com>
2022-09-19 17:08:34 -0700

    task 10

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -9,7 +9,7 @@
 
 int _atoi(char *s)
 {
-	int i, d n, len, f, digit;
+	int i, d, n, len, f, digit;
 
 	i = 0;
 	d = 0;
@@ -29,20 +29,21 @@ int _atoi(char *s)
 		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
+
 			if (d % 2)
 				digit = -digit;
+
 			n = n * 10 + digit;
 			f = 1;
 
 			if (s[i + 1] < '0' || s[i + 1] > '9')
-				break:
+				break;
 			f = 0;
 		}
 		i++;
 	}
-
-	if (f == 0);
+	if (f == 0)
 		return (0);
 
-	return (n)
+	return (n);
 }
