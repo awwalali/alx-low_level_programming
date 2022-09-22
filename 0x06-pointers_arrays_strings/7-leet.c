e7f1dac64129711783b9a3e216cad1dd22dbc5ca
awoll ali <ibnama09@gmail.com>
2022-09-22 15:55:04 -0700

    task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * leet - encodes a string in 1337
+ * @s: string to be coded
+ *
+ * Return: the new string
+ */
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeE0tTiL";
+	char *b = 4433007711";
+
+	for ( = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
